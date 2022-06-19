201f664d73dcd7cb4b7c8b6b083315b182adade5
Jimmy Prada <jipragu@emeal.nttdata.com>
2022-06-19 17:38:47 -0500

    iniciando algunos ajustes

--- src/main/java/com/nttdata/bootcamp/transactiondomain/repository/CreditPaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.bootcamp.transactiondomain.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.nttdata.bootcamp.transactiondomain.model.CreditPayment;
-
-public interface CreditPaymentRepository extends ReactiveMongoRepository<CreditPayment, String>{
-
-}
